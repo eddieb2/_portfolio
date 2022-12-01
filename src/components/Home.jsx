a2498090c28124bcb4bb1a12e815e85cadb40a28
Edward Blanciak <blanciak.edward2@gmail.com>
2022-11-30 20:51:33 -0500

    removed text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,12 +10,7 @@ const Home = () => {
 				<p className='text-red-400'>Hi, my name is</p>
 				<h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Eddie Blanciak</h1>
 				<h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Software Engineer</h2>
-				<p className='text-[#8892b0] py-4 max-w[700px]'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero explicabo iusto
-					accusantium dolore dignissimos officia quisquam voluptatibus omnis, illum
-					quibusdam autem dolorem sed recusandae tempore consectetur ducimus possimus
-					quia. Officiis?
-				</p>
+				<p className='text-[#8892b0] py-4 max-w[700px]'></p>
 				<div>
 					<Link to='projects' smooth={true} duration={500}>
 						<button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-400 hover:font-bold'>
